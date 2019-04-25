618fce31f393fc9cc5f201297e02e415ff455fa4
Thiago Benites <ta.auto.tec@gmail.com>
2019-04-25 09:43:25 -0300

    Layout home

---
+++ 5-views/home.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Ta Atomação - Desenvolvimento de sites</title>
+
+        <link rel="shortcut icon" href="6-img/favicon.ico" />
+        <link rel="stylesheet" href="4-css/bootstrap.min.css" />
+        <link rel="stylesheet" href="4-css/style.css" />
+        
+        <!-- ÍCONES E GIFS UTEIS -->
+        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="6-img/logo.png" alt="TA Automação"></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#about">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#services">Empresa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#portfolio">Projetos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#contact">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#contact">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Masthead -->
+        <header class="masthead">
+            <div class="container h-100">
+                <!-- <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="chamada text-uppercase text-white font-weight-bold">Chegou a hora de ter seu próprio site!</h1>
+                        <hr class="divider my-4">
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="sub-chamada font-weight mb-5">Melhore suas vendas, promova sua imagem, automatize seus processos de negócio. Seja qual for sua necessidade temos a solução!</p>
+                        <a class="btn btn-xl js-scroll-trigger" href="#about">Quero Meu Site</a>
+                    </div>
+                </div> -->
+                <div id="carousel" class="carousel slide text-center" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="col-lg-12 align-self-end">
+                                <h1 class="chamada text-uppercase text-white font-weight-bold">Chegou a hora de ter seu próprio site!</h1>
+                                <hr class="divider my-4">
+                            </div>
+                            <div class="col-lg-12 align-self-baseline">
+                                <p class="sub-chamada font-weight mb-5">Melhore suas vendas, promova sua imagem, automatize seus processos de negócio. Seja qual for sua necessidade temos a solução!</p>
+                                <a class="btn btn-xl js-scroll-trigger" href="#about">Quero Meu Site</a>
+                            </div>                       
+                        </div>
+
+                        <div class="carousel-item">
+                            <div class="col-lg-12 align-self-end">
+                                <h1 class="chamada text-uppercase text-white font-weight-bold">Chegou a hora de ter seu próprio site!</h1>
+                                <hr class="divider my-4">
+                            </div>
+                            <div class="col-lg-12 align-self-baseline">
+                                <p class="sub-chamada font-weight mb-5">Pagina 2</p>
+                                <a class="btn btn-xl js-scroll-trigger" href="#about">Quero Meu Site</a>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+
+        <!-- JAVASCRIPT -->
+        <script src="7-js/jquery-3.3.1.slim.min.js"></script>
+        <script src="7-js/popper.min.js"></script>
+        <script src="7-js/bootstrap.min.js"></script>
+        <script src="7-js/scrollreveal.min.js"></script>
+        
+        <!-- AJAX -->
+        <script src="0-php-framework/Ajax/xhttp.js"></script>
+        <script src="2-ajax/test_functions.js"></script>
+        <script src="2-ajax/generic.js"></script>
+    </body>
+</html>
